0273bcd276159ecdcb626370cc0b72793b401039
MISsungate <florenceforwork08@gmail.com>
2024-10-24 14:30:13 +0800

    Fix port retrieval issue

--- app.py
+++ app.py
@@ -468,6 +468,6 @@ def get_customer_info(name):
     connection.close()
     return customer or {}
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT, 5000"))
-    app.run(host="0.0.0.0", port=port)
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
